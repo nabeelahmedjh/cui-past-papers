01fa431feb03da40035cc7ecbfb894105addc7e3
Nabeel Ahmed <72853909+nabeelahmedjh@users.noreply.github.com>
2023-08-14 22:03:56 +0500

    added models for submission, past paster and contribute also their views and url conf

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
-class Contribution(models.Model):
+class Submission(models.Model):
     name = models.CharField(max_length=255)
     file = models.FileField(upload_to='raw_pdfs/')
     linkedIn = models.URLField(default='')
@@ -12,4 +11,40 @@ class Contribution(models.Model):
     def __str__(self) -> str:
         return self.name
 
+
+class Contributor(models.Model):
+    name = models.CharField(max_length=255)
+    linkedIn = models.URLField(default='')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class PastPapar(models.Model):
+
+    campus_choices = [
+        ('ISB', 'Islamabad'),
+        ('LHR', 'Lahore'),
+        ('SWL', 'Sahiwal'),
+        ('abbottabad', 'abbottabad'),
+        ('WAH', 'Wah'),
+        ('attock', 'attock'),
+        ('vehari', 'vehari'),
+        ('virtual', 'virtual'),
+    ]
+
+    exam_type_choices = [
+        ('M', 'Midterm'),
+        ('T', 'Terminal')
+    ]
+    course_code = models.CharField(max_length=10)
+    course_title = models.CharField(max_length=255)
+    instructor_name = models.CharField(max_length=255)
+    campus = models.CharField(max_length=55, choices=campus_choices)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    exam_type = models.CharField(max_length=1, choices=exam_type_choices)
+    file = models.FileField(upload_to='pastpapers/')
+    submitted_by = models.ForeignKey(Contributor, on_delete=models.SET_NULL)
 
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('contributions/',views.ContributionsView.as_view(), name='contributions')
+    path('submissions/',views.SubmissionView.as_view(), name='submissions'),
+    path('submissions/<int:pk>',views.SubmissionDetailView.as_view(), name='submission-detail'),
+    path('past-papers/',views.PaperPaperView.as_view(), name='past-papers'),
+
+
+    path('contributions/', views.ContributionsView.as_view(), name='contributions')
+    # path('submissions/',views.ContributionsView.as_view(), name='submissions')
 ]

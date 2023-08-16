5d343c3b12fdb89d309d39490150f1182c82968e
Nabeel Ahmed <72853909+nabeelahmedjh@users.noreply.github.com>
2023-08-16 17:38:51 +0500

    added cords allowed origins

--- backend/cui_past_papers/settings.py
+++ backend/cui_past_papers/settings.py
@@ -40,11 +40,15 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'base',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,3 +131,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://domain.com",
+    "http://localhost:5173",
+]

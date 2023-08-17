7924276aa9640650c8d83af26a175f43445e4c12
Nabeel Ahmed <72853909+nabeelahmedjh@users.noreply.github.com>
2023-08-17 12:41:27 +0500

    removed a type in submission view

--- backend/base/views.py
+++ backend/base/views.py
@@ -32,7 +32,7 @@ class SubmissionView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
 

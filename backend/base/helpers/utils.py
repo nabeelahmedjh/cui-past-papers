bdd8826e402a1201b35fcfb67cb16d7f9cc7341e
Nabeel Ahmed <72853909+nabeelahmedjh@users.noreply.github.com>
2023-08-25 16:38:20 +0500

    added paginateResponse and getORderByFields helper methods

--- backend/base/helpers/utils.py
+++ backend/base/helpers/utils.py
@@ -28,3 +28,30 @@ def deleteSubmission(submission_id):
             return ({
             'message': "Past paper doesn't exists"
         }, status.HTTP_404_NOT_FOUND)
+
+
+
+def getOrderByFields(order_by_param, allowed_fields, default_field='name'):
+        
+        order_by_fields = order_by_param.split(',')
+        order_by_fields = [field for field in order_by_fields if field.lstrip('-') in allowed_fields]
+
+        if not order_by_fields:
+            order_by_fields = [default_field]
+        
+        return order_by_fields
+
+
+def paginateResponse(request, queryset, pagination_class, serializer):
+
+    paginator = pagination_class()
+    paginated_queryset = paginator.paginate_queryset(queryset, request)
+    serializer = serializer(paginated_queryset, many=True)
+
+    response_data = {
+        'count': paginator.page.paginator.count,
+        'previous': paginator.get_previous_link(),
+        'next': paginator.get_next_link(),
+        'results': serializer.data
+    }
+    return response_data

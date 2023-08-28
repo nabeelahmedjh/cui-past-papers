13ff1d4a0f99ae5244affb393ce3f887ee97af76
shahzaib-ai <shahzaib1234@outlook.com>
2023-08-28 12:03:17 +0500

    Upgraded the PaperViewer to use dynamic routes

--- frontend/past-papers/src/components/PastPapers/PaperViewer.tsx
+++ frontend/past-papers/src/components/PastPapers/PaperViewer.tsx
@@ -1,12 +1,30 @@
 import AdobePDF from "../ReviewSubmissions/AdobePdf";
 
-import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function PaperViewer() {
-  const course = useLocation().state;
-  const url = `${import.meta.env.VITE_DJANGO_SERVER_URL}${course.file}` || "";
-  console.log(course);
-  console.log(url);
+  const [paperData, setPaperData] = useState();
+  const [url, setUrl] = useState(``);
+
+  const paperId = useParams().id;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(`/past-papers/${paperId}`);
+        setPaperData(response.data);
+        setUrl(
+          `${import.meta.env.VITE_DJANGO_SERVER_URL}${response.data.file}`
+        );
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, [paperId]);
 
   const campusChoices = {
     ISB: "Islamabad",
@@ -35,12 +53,37 @@ export default function PaperViewer() {
   }
 
   return (
-    <>
-      <h1>PDf Viewer</h1>
-      <a href={url} target="_blank">
-        Download PDF
-      </a>
-      {course && <AdobePDF url={url} />}
-    </>
+    paperData && (
+      <>
+        <div className="flex justify-center">
+          <div className="flex flex-col gap-4 border-2 rounded-sm p-2">
+            <div className="flex gap-2">
+              <p>
+                {paperData.course_code.toUpperCase()}, {paperData.course_title}
+              </p>
+            </div>
+            <div className="flex gap-2">
+              <p className="capitalize">{paperData.instructor_name}</p>
+            </div>
+            <div className="flex gap-2">
+              <p>{getExamTypeName(paperData.exam_type)}</p>
+              <p>{paperData.year}</p>
+              <p>{getCampusName(paperData.campus)}</p>
+            </div>
+            <div className="flex gap-2">
+              <p>
+                <a className="hover:text-primary" href={url} target="_blank">
+                  Download PDF
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="sm:w-[90%] m-[5%] ">
+          <AdobePDF url={url} />
+        </div>
+      </>
+    )
   );
 }

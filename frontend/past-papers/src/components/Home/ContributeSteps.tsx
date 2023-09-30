f70ae7370cc7e73c61460f3a983832b3bd924bf2
shahzaib-ai <shahzaib1234@outlook.com>
2023-09-30 11:30:38 +0500

    Created Contributon Steps Timeline Section

--- frontend/past-papers/src/components/Home/ContributeSteps.tsx
+++ frontend/past-papers/src/components/Home/ContributeSteps.tsx
@@ -1,3 +1,24 @@
+import "./Timeline.css";
+
 export default function ContributeSteps() {
-  return <div>ContributeSteps</div>;
+  return (
+    <>
+      <h1 className="max-w-5xl mx-auto text-4xl text-center mt-32 mb-16">
+        Contribute in 3 steps
+      </h1>
+      <div className="flex justify-center">
+        <div className="vtl">
+          <div className="circle">
+            <p className="txt">Click add submission </p>
+          </div>
+          <div className="circle">
+            <p className="txt">Fill the form</p>
+          </div>
+          <div className="circle">
+            <p className="txt">Submit</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
